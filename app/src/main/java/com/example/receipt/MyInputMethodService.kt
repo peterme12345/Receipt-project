9199202d117344255db5f08528ada500a87b88c3
peterme12345 <npeter389@gmail.com>
2023-12-29 02:03:42 -0800

    second commit

--- app/src/main/java/com/example/receipt/MyInputMethodService.kt
+++ app/src/main/java/com/example/receipt/MyInputMethodService.kt
@@ -1,39 +1,87 @@
 package com.example.receipt
 
+
+import android.R
 import android.inputmethodservice.InputMethodService
+import android.inputmethodservice.Keyboard
 import android.inputmethodservice.KeyboardView
+import android.text.TextUtils
+import android.view.View
+import android.view.KeyEvent
+
 
 class MyInputMethodService: InputMethodService(),KeyboardView.OnKeyboardActionListener {
+
+    private var keyboardView: KeyboardView? = null
+    private var keyboard: Keyboard? = null
+    private var caps = false
     override fun onPress(primaryCode: Int) {
-        TODO("Not yet implemented")
     }
 
     override fun onRelease(primaryCode: Int) {
-        TODO("Not yet implemented")
     }
 
     override fun onKey(primaryCode: Int, keyCodes: IntArray?) {
-        TODO("Not yet implemented")
+        val inputConnection = currentInputConnection
+        if (inputConnection != null) {
+            when (primaryCode) {
+                Keyboard.KEYCODE_DELETE -> {
+                    val selectedText = inputConnection.getSelectedText(0)
+                    if (TextUtils.isEmpty(selectedText)) {
+                        inputConnection.deleteSurroundingText(1, 0)
+                    } else {
+                        inputConnection.commitText("", 1)
+                    }
+                    caps = !caps
+                    keyboard!!.setShifted(caps)
+                    keyboardView!!.invalidateAllKeys()
+                }
+
+                Keyboard.KEYCODE_SHIFT -> {
+                    caps = !caps
+                    keyboard!!.setShifted(caps)
+                    keyboardView!!.invalidateAllKeys()
+                }
+
+                Keyboard.KEYCODE_DONE -> inputConnection.sendKeyEvent(
+                    KeyEvent(
+                        KeyEvent.ACTION_DOWN,
+                        KeyEvent.KEYCODE_ENTER
+                    )
+                )
+
+                else -> {
+                    var code = primaryCode.toChar()
+                    if (Character.isLetter(code) && caps) {
+                        code = code.uppercaseChar()
+                    }
+                    inputConnection.commitText(code.toString(), 1)
+                }
+            }
+        }
     }
 
     override fun onText(text: CharSequence?) {
-        TODO("Not yet implemented")
     }
 
     override fun swipeLeft() {
-        TODO("Not yet implemented")
     }
 
     override fun swipeRight() {
-        TODO("Not yet implemented")
     }
 
     override fun swipeDown() {
-        TODO("Not yet implemented")
     }
 
     override fun swipeUp() {
-        TODO("Not yet implemented")
+    }
+
+    override fun onCreateInputView(): View {
+        keyboardView = layoutInflater.inflate(R.layout.keyboard_view, null) as KeyboardView
+        keyboard = Keyboard(this, R.xml.keys_layout)
+        keyboardView!!.keyboard = keyboard
+        keyboardView!!.setOnKeyboardActionListener(this)
+        return keyboardView!!
     }
 
 }
--- app/src/main/java/com/example/receipt/ui/theme/MainActivity.kt
+++ app/src/main/java/com/example/receipt/ui/theme/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.receipt
+package com.example.receipt.ui.theme
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -10,7 +10,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.receipt.ui.theme.ReceiptTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
